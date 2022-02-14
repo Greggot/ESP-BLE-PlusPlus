b239e42765bec46f6b7bc8d021a3a1b95e897918
Greggot <alexgerggot@gmail.com>
2022-02-14 13:15:33 +0300

    Add Perm and Prop namespaces with bit enums
      Enums replace long macros which make characteristic
    initialization too long to fit in a single string.
      It is much simplier to write 'Prop::Read | Prop::Write' then
    'ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE'.

--- inc/Bluetooth.hpp
+++ inc/Bluetooth.hpp
@@ -19,6 +19,37 @@
 
 typedef __uint8_t byte;
 
+namespace Perm
+{
+    enum bit
+    {
+        Read            = 1,
+        ReadEncrypted   = (1 << 1),
+        ReadEncrMitm    = (1 << 2),
+        Write           = (1 << 4),
+        WriteEncrypted  = (1 << 5),
+        WriteEncrMitm   = (1 << 6),
+        WriteSigned     = (1 << 7),
+        WriteSignMitm   = (1 << 8),
+        ReadAuth        = (1 << 9),
+        WriteAuth       = (1 << 10),
+    };
+}
+namespace Prop
+{
+    enum bit
+    {
+        Broadcast   = 1,
+        Read        = (1 << 1),
+        WriteNr     = (1 << 2),
+        Write       = (1 << 3),
+        Notify      = (1 << 4),
+        Indicate    = (1 << 5),
+        Auth        = (1 << 6),
+        ExtProp     = (1 << 7),
+    };
+}
+
 class GATTinstance
 {
     protected:
