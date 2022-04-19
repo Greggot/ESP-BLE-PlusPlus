e523e1e182ca05bba6b52c61b6c530b031f8e692
Greggot <alexgerggot@gmail.com>
2022-04-19 13:24:10 +0300

    Finish Service refactoring, Add constructor

--- inc/Bluetooth.hpp
+++ inc/Bluetooth.hpp
@@ -124,17 +124,16 @@ class Characteristic : public GATTinstance
 class Service : public GATTinstance
 {
     private:
-        esp_gatt_srvc_id_t service_id;
+        esp_gatt_srvc_id_t serviceID;
         
         const std::vector<Characteristic*> Characteristics;
         const size_t CharacteristicsSize;
     public:
-        uint16_t CharCounter = 0;
-
         const std::vector<Characteristic*> getCharacteristics() const { return Characteristics; }
         size_t getCharacteristicsSize() const { return CharacteristicsSize; }
 
         Service(const uint32_t UUID, const std::vector<Characteristic*> Characteristics);
+        Service(uint8_t UUID[ESP_UUID_LEN_128], const std::vector<Characteristic*> Characteristics);
 
         void Start();
         void Create();
--- src/Bluetooth_Service.cpp
+++ src/Bluetooth_Service.cpp
@@ -3,14 +3,22 @@
 Service::Service(const uint32_t ID, const std::vector<Characteristic*> Characteristics)
     : GATTinstance(ID), Characteristics(Characteristics), CharacteristicsSize(Characteristics.size())
 {
-    service_id.is_primary = true;
-    service_id.id.inst_id = 0x00;
-    service_id.id.uuid = UUID;
+    serviceID.is_primary = true;
+    serviceID.id.inst_id = 0x00;
+    serviceID.id.uuid = UUID;
+}
+
+Service::Service(uint8_t ID[ESP_UUID_LEN_128], const std::vector<Characteristic*> Characteristics)
+    : GATTinstance(ID), Characteristics(Characteristics), CharacteristicsSize(Characteristics.size())
+{
+    serviceID.is_primary = true;
+    serviceID.id.inst_id = 0x00;
+    serviceID.id.uuid = UUID;
 }
 
 void Service::Create()
 {
-    esp_ble_gatts_create_service(GATTinterface, &service_id, 4 + (CharacteristicsSize << 1));
+    esp_ble_gatts_create_service(GATTinterface, &serviceID, 4 + (CharacteristicsSize << 1));
 }
 
 void Service::Start()
